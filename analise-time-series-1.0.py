8096425f95f0c1a971d804bbf7c1319e31e3da39
leonardobrunnet <leon@if.ufrgs.br>
2021-09-20 18:18:11 -0300

    -included particle class, particle atributes x,y,vx,vy are lists contaning all snapshots -included box class, contains list of list of particles on each box at each time -included calculus of density distribution -include calculus of the Vicsek parameter phi distribution

--- analise-time-series-1.0.py
+++ analise-time-series-1.0.py
@@ -4,6 +4,7 @@ import os
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 def init():
     path="data/"   #files of interest are in data directory just below
@@ -17,9 +18,6 @@ def init():
     print("\n -> Analyzing file: %s\n"%files_in_dir[index])
     input_file_name=path+files_in_dir[index]
     return(path,input_file_name)
-
-
-
 
 
 def verifica(pergunta,n_files):
@@ -41,11 +39,13 @@ def snapshot_count(input_file_name):
             break #EOF
         line_splitted = line.split()
         if line_splitted[0] == "TIME:" :
+            size=int(line_splitted[20].split(".")[0])
+            v0=float(line_splitted[23])
             snapshot_counter += 1
     print(" -> Found %d snaphots. Analyzing..."%snapshot_counter)
     input_file.close()
     ### You may improve the lines above to choose the snapshots you want
-    return snapshot_counter
+    return snapshot_counter,size,v0
 
 
 
@@ -85,16 +85,99 @@ def phi_calc(global_matrix, snapshot_counter):
             num+=1
         phi.append(np.sqrt(vx**2 + vy**2)/num)
     x = np.arange(snapshot_counter)
+    phi = np.array(phi)
+    phimed = np.mean(phi)
+    phistd = np.std(phi)
+    print("Average phi=%.3f, standard dev=%.4f"%(phimed,phistd))
     plt.scatter(x,phi)
+    phimed_list=[]
+    list(phimed_list.append(phimed) for i in range(snapshot_counter))
+    label="phi_av=%.4f"%phimed
+    plt.plot(x,phimed_list,label=label)
+    plt.xlabel("time")
+    plt.ylabel("phi")
+    plt.legend()
+    plt.show()
+    return
+
+def density_distribution(nt,snapshot_counter,number_particles):
+    #calculate average particle density along the space
+    rho_av = np.zeros(nt)
+    for i in range(snapshot_counter):
+        for j in box:
+            rho_av[j.index]+=len(j.mylist[i])
+
+    rho_av/=(lbox**2*snapshot_counter)
+    legend=("Average density = {0:.3f} ".format(number_particles/size**2))
+    sns.distplot(rho_av, hist =True, bins=20, kde=True, color='#1F78B4', label=legend)
+    plt.title("Density distribution")
+    plt.legend()
+    plt.show()
+    return
+
+def phi_time_average(nt,snapshot_counter):
+    phi_av_time=np.zeros(nt)
+    for i in range(snapshot_counter):
+        for j in box:
+            vx,vy=0,0
+            for k in j.mylist[i]:
+                vx+=part[k].vx[i]
+                vy+=part[k].vy[i]
+            phi_av_time[j.index]+=np.sqrt(vx**2+vy**2)/(v0*len(j.mylist[i]))
+
+    phi_av_time/=snapshot_counter
+    phi_av_time_space=np.sum(phi_av_time)/nt
+    legend=(r"Average $\phi$ = {0:.3f} ".format(phi_av_time_space))
+    sns.distplot(phi_av_time, hist=True, bins=20, kde=True, color='#1F78B4', label=legend)
+    plt.title(r" $\phi$ distribution")
+    plt.legend()
     plt.show()
     return
 
 
+
+
+# def msd(global_matrix,snapshot_counter):
+#     glob=np.array(global_matrix)
+#     for i in range(snapshot_counter - 1 ):
+#         for j in range(glob[i,:].size):
+#             print((glob[i+1][2]-glob[i][2])**2+(glob[i+1][3]-glob[i][3])**2)
+#     return
+
+#Particle class definition
+class particle:
+    def __init__(self, index, x, y, vx, vy, theta ):
+        self.index = index
+        self.x=x   #these are np arrays for all snapshots of each particle
+        self.y=y
+        self.vx=vx
+        self.vy=vy
+        self.theta=theta
+        self.Mybox=[]
+
+    def mybox(self,lbox,nx): #each particle calculates the box it is in at the different snaphots
+        aux=(np.array(list(map(int,self.x[:]/lbox)))+nx*np.array(list(map(int,self.y[:]/lbox))))
+        self.Mybox=list(aux)
+        return 
+                
+#Box class definition
+class boite:
+    def __init__(self,index,snapshot_counter):
+        self.index = index
+        self.mylist = list( [] for i in range(snapshot_counter))
+        return
+
 #main program
 path,input_file_name=init()
 
-## Here we look for the number of system snapshots
-snapshot_counter=snapshot_count(input_file_name)
+## Here we look for the number of system snapshots and system size
+snapshot_counter,size,v0=snapshot_count(input_file_name)
+
+#define the box size used to analyse
+lbox=10
+nx=int(size/lbox)+1
+nt=nx**2
+
 
 #Reopen file, read all data and put it on global_matrix
 global_matrix=construct_global_matrix(input_file_name,snapshot_counter)
@@ -102,7 +185,43 @@ global_matrix=construct_global_matrix(input_file_name,snapshot_counter)
 #print all data after reading
 #printall(global_matrix)
         
-#calculus of vicseck order parameter and graphics at each snapshot
-phi_calc(global_matrix,snapshot_counter)
+#calculus of vicsek order parameter and graphics at each snapshot
+#phi_calc(global_matrix,snapshot_counter)
 
-    
+#msd calculus
+#msd(global_matrix,snapshot_counter)
+
+
+glob_array=np.array(global_matrix) #I suppose the number of particles will not vary, so I trasform to numpy array
+
+#construct particle class and box class; each particle has arrays of x,y,vx,vy with values for all snapshots
+snapshot_counter,number_particles,dyn_var=glob_array.shape
+part=list(particle(i,glob_array[:,i,0],glob_array[:,i,1],glob_array[:,i,4],glob_array[:,i,5],glob_array[:,i,6]) for i in range(number_particles))
+
+
+#define the boxes as an object list                                                       
+box=list(boite(i,snapshot_counter) for i in range(nt))
+
+#Discover the boxes each particle passes and put in an array
+#map(lambda i:i.mybox(lbox,nx), part)
+for i in part:
+    i.mybox(lbox,nx)
+
+#Construct the particle population on each box at each time by particle index
+for i in range(snapshot_counter):
+    for j in part :
+        k=int(j.Mybox[i])
+        if k > nt :
+            print("No box for this particle! Check the coordinates and system size.")
+            print(k,j.Mybox[i],nt)
+            exit()
+        else:
+            box[k].mylist[i].append(j.index)
+            
+#calculate particle density distribution averaged over snapshots
+#density_distribution(nt,snapshot_counter,number_particles)
+
+
+
+#phi distribution averaged over snapshots
+phi_time_average(nt,snapshot_counter)
